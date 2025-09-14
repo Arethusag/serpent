780560517d992d583f3c0d5dce263bd84062b727
Matt Marcoux <mattmarcoux@gmail.com>
2025-09-14 13:04:08 -0700

    build update

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cc -D_POSIX_C_SOURCE=199309L -Wall -Werror -Wno-error=unused-but-set-variable -Wno-error=switch -Wno-switch -Wpedantic -ansi -g -o build/main main.c
+cc -D_POSIX_C_SOURCE=199309L -Wall -Werror -Wno-error=unused-but-set-variable -Wno-error=switch -Wno-switch -Wpedantic -ansi -g -o build/snake snake.c
